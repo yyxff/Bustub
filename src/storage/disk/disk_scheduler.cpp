08f8360160dc0e60076d651b82f69d16fcc091a1
yyxff <salmontwosday@gmail.com>
2025-03-20 23:53:11 -0400

    passed disk scheduler

--- src/storage/disk/disk_scheduler.cpp
+++ src/storage/disk/disk_scheduler.cpp
@@ -18,9 +18,9 @@ namespace bustub {
 
 DiskScheduler::DiskScheduler(DiskManager *disk_manager) : disk_manager_(disk_manager) {
   // TODO(P1): remove this line after you have implemented the disk scheduler API
-  throw NotImplementedException(
-      "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
-      "throw exception line in `disk_scheduler.cpp`.");
+  // throw NotImplementedException(
+  //     "DiskScheduler is not implemented yet. If you have finished implementing the disk scheduler, please remove the "
+  //     "throw exception line in `disk_scheduler.cpp`.");
 
   // Spawn the background thread
   background_thread_.emplace([&] { StartWorkerThread(); });
@@ -41,7 +41,11 @@ DiskScheduler::~DiskScheduler() {
  *
  * @param r The request to be scheduled.
  */
-void DiskScheduler::Schedule(DiskRequest r) {}
+void DiskScheduler::Schedule(DiskRequest r) {
+  std::optional<DiskRequest> ele;
+  ele.emplace(std::move(r));
+  request_queue_.Put(std::move(ele));
+}
 
 /**
  * TODO(P1): Add implementation
@@ -51,6 +55,21 @@ void DiskScheduler::Schedule(DiskRequest r) {}
  * The background thread needs to process requests while the DiskScheduler exists, i.e., this function should not
  * return until ~DiskScheduler() is called. At that point you need to make sure that the function does return.
  */
-void DiskScheduler::StartWorkerThread() {}
+void DiskScheduler::StartWorkerThread() {
+  while(1){
+    auto ele = request_queue_.Get();
+    if (!ele.has_value()){// if it is a null ptr signal, quit
+      return;
+    }
+
+    auto & r = ele.value();
+    if (r.is_write_){
+      disk_manager_->WritePage(r.page_id_, r.data_);
+    }else{
+      disk_manager_->ReadPage(r.page_id_, r.data_);
+    }
+    r.callback_.set_value(true);
+  }
+}
 
 }  // namespace bustub
